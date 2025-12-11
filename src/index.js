8a3b31448b0084f34c7e5d6ca19e230951c36899
Itsfermar <fermar.signar@nmsc.edu.ph>
2025-12-11 19:19:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';        // Global CSS styles
+import App from './App';     // Main App component
+import reportWebVitals from './reportWebVitals'; // Performance measuring tool
 
+// Create the root element where the entire React app will be rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside <React.StrictMode>
+// StrictMode helps highlight potential problems in development
 root.render(
   <React.StrictMode>
-    <App />
+    <App />   {/* Main component that holds your whole application */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Measure app performance
+// You can pass a function to log results (example: reportWebVitals(console.log))
+// or send to an analytics endpoint
 reportWebVitals();
